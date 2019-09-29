732b4dd0b28ef245ceed36011974d43a09c6eeb7
Student <example@example.com>
2019-09-29 14:17:22 -0700

    user register and login both work with bcrpyt now

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const bcrypt = require('bcryptjs');
-const { registerValidation } = require('../validation')
+const { registerValidation, loginValidation } = require('../validation')
 
 //this has to be async because it takes a second to pull this information fronm the DB
 router.post('/register', async (req, res) => {
@@ -27,14 +27,28 @@ router.post('/register', async (req, res) => {
     //always put any sort of async call to a DB or API in a try/catch block
     try {
         const savedUser = await user.save();
-        res.send(savedUser)
+        //this now returns only the user id instead of the only user object
+        res.send({ user: user._id })
     } catch (err) {
         res.status(400).send(err)
     }
 });
 
-module.exports = router;
+router.post('/login', async (req, res) => {
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+    //checking if the user already exists
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Email or password is wrong');
 
+    //check if password is correct
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send("invalid password");
+    res.send('success!')
+})
+
+module.exports = router;
+//find out where the user ID is even coming from
 //have to run this command to use joi.validate
 //npm uninstall --save @hapi/joi
 //npm install--save @hapi/joi@15.0.3

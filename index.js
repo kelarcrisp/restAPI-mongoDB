5f934ee05ade56fcdb5cb70ce75572b2583c6954
Kelar Crisp <kelcrisp007@icloud.com>
2019-09-29 17:06:14 -0700

    added comments in index

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ app.listen(3000, () => console.log('server up and running'));
 
 
 
+// -------- NOTES --------
+ // express--use .get to just recieve info, .post() to send info. you will call this method on the variable you amde that is invoking express. theres also .delete(), and .patch(). the first parameter on each of this is just the route that you want this method to be invoked on. each of these will always come with a callback function as a second parameter that will take (req,res) 
+
+
+ // MIDDLEWARE is just a function that runs when a specific route is hit. create this with .use() ---look into this more--
+
+
+ //mongoDB -- mongoose makes conencting to a DB very easy. connect to DB with importing mongoose, and then call .connect('URL') on the mongoose variable you make. make sure to make an .env file to store the pass in. always test this connect with adding a second parameter with a call back fucntion that contains a console.log
+
